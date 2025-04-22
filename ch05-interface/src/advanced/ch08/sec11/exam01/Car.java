42eb84ce5a3fc5cdd3a20de98967823c6b397be4
kang-yesung <rkddptjd5228@gmail.com>
2025-04-22 18:21:11 +0900

    25.04.22

--- ch05-interface/src/advanced/ch08/sec11/exam01/Car.java
+++ ch05-interface/src/advanced/ch08/sec11/exam01/Car.java
@@ -1,9 +1,12 @@
 package advanced.ch08.sec11.exam01;
 
 public class Car {
-    public  Tire tire;
+    public  Tire tire1 = new HankookTire();
+    public  Tire tire2 = new HankookTire();
 
     public void run() {
-        tire.roll();
+
+        tire1.roll();
+        tire2.roll();
     }
 }
--- ch05-interface/src/advanced/ch08/sec11/exam01/CarExample.java
+++ ch05-interface/src/advanced/ch08/sec11/exam01/CarExample.java
@@ -3,12 +3,11 @@ package advanced.ch08.sec11.exam01;
 public class CarExample {
     public static void main(String[] args) {
         Car mycar = new Car();
-        mycar.tire = new HankookTire();
-        mycar.run();
         mycar.run();
 
-        mycar.tire = new KumhoTire();
+        mycar.tire1 = new KumhoTire();
+        mycar.tire2 = new KumhoTire();
         mycar.run();
-        mycar.run();
+
     }
 }
